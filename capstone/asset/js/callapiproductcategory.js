0c2d3c73d4a40384fd76adb2d24b88a608d7c123
Vinh Nguyen <motmuahaxinhdep@gmail.com>
2023-05-03 15:25:53 +0700

    call api success

---
+++ capstone/asset/js/callapiproductcategory.js
@@ -0,0 +1,37 @@
+function displayRealateProduct(){
+    var promise=axios({
+        url:'https://shop.cyberlearn.vn/api/Product',
+        method:'GET',
+    })
+    promise.then(function(result){
+        renderRelateProduct(result.data.content);
+    })
+    promise.catch(function(error){
+        console.log(error)
+    })
+}
+
+function renderRelateProduct(arrRP){
+    var category='';
+    for (var index=0;index<arrRP.length;index++)
+    {
+        var  categoryRP=arrRP[index];
+        category+= `
+        <div class="product">
+          <div class="product-img">
+          <img src="${categoryRP.image}" alt="">
+          </div>
+          <div class="product-info">
+            <h3 class="product-title">${categoryRP.name}</h3>
+            <p class="product-desc">${categoryRP.shortDescription}</p>
+            <div class="product-bottoms">
+              <button class="product-btn"><a href="./detail.html?productid=${categoryRP.id}">Buy now </a></button>
+              <button class="product-btns">85$</button>
+            </div>
+          </div>
+        </div>
+        `
+    }
+    document.querySelector('#relateProduct').innerHTML=category;
+}
+displayRealateProduct()
--- capstone/asset/js/detail.js
+++ capstone/asset/js/detail.js
@@ -4,70 +4,58 @@ window.onload = function () {
     console.log('params',myParam)
 
     //call api load lên giao diện 
+        var promise=axios({
+            url:'https://shop.cyberlearn.vn/api/Product/getbyid?id='+myParam,
+            method:'GET',
+            ResponseType:JSON,
+        })
+            promise.then(function(result){
+                console.log(result.data.content     );
+                
+                var contentClick=result.data.content;
+                var arrSize=contentClick.size
+                var displaySize=``;
+                var indexSize=-1
+                for(var index=0;index<arrSize.length;index++)
+                {
+                    indexSize=index
+                    displaySize+=`
+                        <button class="btnsize" >
+                        ${arrSize[indexSize]}
+                        </button>
+                    `
+                }
+                var display='';
+                display=`<section class="product-detail">
+                <div class="photo">
+                    <img src="${contentClick.image}">
+                </div>
+                <div class="detail">
+                    <h1> ${contentClick.name}</h1>
+                   
+                    <span>Available size</span>
+                <div class="catalogue-size">
+                    ${displaySize}
+                </div>
+                <h3>${contentClick.price}</h3>
+                <div class="changethenumber">
+                    <button class="" id="btn-up">+</button>
+                    <span id="number"></span>
+                    <button class="" id="btn-down">-</button>
+                </div>
+                <button class="btn" id="btn-add-to-cart">Add to cart</button>
+                </div>
+                
+            </section>
     
-}
-function displayDetail(arrCategoryShoes){
-    var indexID=-1;
-    for (var index=0;index<arrCategoryShoes.length;index++)
-    {
-        var typeShoe=arrCategoryShoes[index];
-        if (typeShoe.id===idDetail)
-        {
-            indexID=index;break;
-        } 
-
-        if (indexID!==-1){
-           renderDetail(typeShoe[indexID])
-        }
+    
+                `
+                
+                document.querySelector('.container').innerHTML=display;
+            })
+            promise.catch(function(error){
+                console.log(error)
+            })
     }
-}
-
-function detailShoe(){
-    var promise=axios({
-        url:'https://shop.cyberlearn.vn/api/Product/getbyid?id='+myParam,
-        method:'GET',
-        ResponseType:JSON,
-    })
-    promise.then(function(result){
+            
         
-        var arrayDetail=result.data.content;
-        displayDetail(arrayDetail);
-    })
-    promise.catch(function(error){
-        console.log(error)
-    })
-}
-
-function renderDetail(arrCategoryShoes){
-    var display=``;
-    var displayarrSize=``;
-    for(var index=0;index<arrCategoryShoes.size.length;index++){
-        var displaySize=``;
-        displaySize+=`<button class="btn">
-        ${arrCategoryShoes.size[index]}
-        </button>`
-    }
-    displayarrSize=`<div class="catalogue-size"> ${displaySize}</div>
-    `
-    display=`
-    <section class="product-detail">
-            <div class="photo">
-                <img src="${arrCategoryShoes.image}">
-            </div>
-            <div class="detail">
-                <h1>${arrCategoryShoes.name}</h1>
-               <div class="descriptionDetail">${arrCategoryShoes.description}</div>
-                <span>Available size</span>
-            <div class="catalogue-size">
-                ${displayarrSize};
-            </div>
-            <h3>${arrCategoryShoes.price}</h3>
-            <div class="changethenumber">
-                <button class="" id="btn-up">+</button>
-                <span id="number"></span>
-                <button class="" id="btn-down">-</button>
-            </div>
-            <button class="btn" id="btn-add-to-cart">Add to cart</button>
-            </div>
-`
-}
